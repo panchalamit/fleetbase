9afd59a6b682df3e0b07dad1e6a0aaf51b2b4660
Prasad Oturkar <prasad.oturkar@attomdata.com>
2021-12-27 18:15:27 +0530

    initial commit

---
+++ src/Places.php
@@ -0,0 +1,39 @@
+<?php 
+namespace Fleetbase;
+
+require '../vendor/autoload.php';
+
+
+
+class Places {
+     /**
+     * get IP address of the user
+     *
+     * @return void
+     */
+    public function list()
+    {
+        $headers= array('Authorization' => 'Bearer flb_live_6pHEcNFoSKHLFqsWc3Sm');
+
+        $client = new \GuzzleHttp\Client([
+            'headers' => $headers
+        ]);
+
+        
+        // $client = new \GuzzleHttp\Client([
+        //     'headers' => $headers
+        // ]);
+ 
+        $r = $client->request('GET', 'https://api.fleetbase.io/v1/drivers');
+        $response = $r->getBody()->getContents();
+        
+        echo "working";
+        echo $response;die;
+    }
+}
+
+// $a = new Places();
+// $a->list();
+
+
+?>
