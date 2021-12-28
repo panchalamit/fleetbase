1670ce81c9672fd6c32f8c9c428495d2dd9ef322
Prasad Oturkar <prasad.oturkar@attomdata.com>
2021-12-28 16:14:11 +0530

    working

--- src/Places.php
+++ src/Places.php
@@ -7,8 +7,10 @@ require_once $basepath.'/vendor/autoload.php';
 
 
 class Places {
-    //
-    public function index() 
+    /*
+       Get drivers list api
+    */
+    public function view() 
     {
         // $bearerToken = $request->bearerToken();
         // $headers= array('Authorization' => 'Bearer'.$bearerToken);
@@ -21,14 +23,16 @@ class Places {
 
         $r = $client->request('GET', 'https://api.fleetbase.io/v1/drivers');
         $response = $r->getBody()->getContents();
-
+       
         return $response;
     }
 
-    //
+    /*
+       Create new driver api
+    */
     public function create($request) 
     {
-        $params = $request->input();
+        $params = $request;
         $headers= array('Authorization' => 'Bearer flb_live_6pHEcNFoSKHLFqsWc3Sm');
 
         $client = new \GuzzleHttp\Client([
@@ -37,14 +41,68 @@ class Places {
        
         $r = $client->request('POST', 'https://api.fleetbase.io/v1/drivers', ['form_params' =>$params]);
         $response = $r->getBody()->getContents();
+        
+        return $response;
+    }
 
+      /*
+       find one particular driver record
+    */
+    public function findOne($request) 
+    {
+        $driverId = $request->id;
+        // $headers= array('Authorization' => 'Bearer'.$bearerToken);
+        $headers= array('Authorization' => 'Bearer flb_live_6pHEcNFoSKHLFqsWc3Sm');
+        
+        $client = new \GuzzleHttp\Client([
+            'headers' => $headers
+        ]);
+
+        $r = $client->request('GET', 'https://api.fleetbase.io/v1/drivers/'.$driverId);
+        $response = $r->getBody()->getContents();
+
+        return $response;
+    }
+
+    /*
+       delete driver record api
+    */
+    public function delete($request) 
+    {
+        $driverId = $request->id;
+        // $headers= array('Authorization' => 'Bearer'.$bearerToken);
+        $headers= array('Authorization' => 'Bearer flb_live_6pHEcNFoSKHLFqsWc3Sm');
+
+        $client = new \GuzzleHttp\Client([
+            'headers' => $headers
+        ]);
+
+        $r = $client->request('DELETE', 'https://api.fleetbase.io/v1/drivers/'.$driverId);
+        $response = $r->getBody()->getContents();
+
+        return $response;
+    }
+
+    /*
+       update driver record api
+    */
+    public function update($request) 
+    {
+        $driverId = $request->id;
+
+        // $headers= array('Authorization' => 'Bearer'.$bearerToken);
+        $headers= array('Authorization' => 'Bearer flb_live_6pHEcNFoSKHLFqsWc3Sm');
+        $client = new \GuzzleHttp\Client([
+            'headers' => $headers
+        ]);
+        $request = $client->request('PUT','https://api.fleetbase.io/v1/drivers/'.$driverId, ['form_params' => $request]);
+        $response = $request->getBody()->getContents();
+        
         return $response;
     }
     
 }
-
-// $a = new Places();
-// $a->list();
-
+$places = new Places();
+$places->list();
 
 ?>
